2dc0efe55b79f59a7235195699f11e029d9e721e
Jeydz <jdjose819@gmail.com>
2024-02-25 22:30:12 +0800

    nav active logic added

--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
-        <!-- Sidebar -->
+         <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
@@ -13,7 +13,7 @@
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "index") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
@@ -27,22 +27,22 @@
                 SALES
             </div>
 
-            <li class="nav-item">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "deliveryreceipts") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="deliveryreceipts.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Delivery Receipts</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "collections") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="collections.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Collections</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "reports") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="reports.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Reports</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "changerequest") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="changerequest.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Change Requests</span></a>
@@ -57,13 +57,13 @@
                 REFERENCES
             </div>
 
-            <li class="nav-item">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "customers") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="customers.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Customers</span></a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "products") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="products.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Products</span></a>
@@ -72,7 +72,7 @@
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
-            <li class="nav-item">
+            <li class="nav-item <?php echo stripos($_SERVER['PHP_SELF'], "accounts") !== FALSE ? "active" : "" ?>">
                 <a class="nav-link" href="accounts.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Accounts</span></a>
